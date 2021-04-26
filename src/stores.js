c1415bcc1171b24326a3cd3deb2bfac7f87b9ed8
Maxime Gauduin <alucryd@archlinux.org>
2021-04-26 12:55:45 +0200

    make the webview a lot prettier

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,18 @@
 import { writable } from 'svelte/store';
 
 export const systems = writable([]);
+export const systemId = writable(-1);
 export const games = writable([]);
+export const gameId = writable(-1);
 export const roms = writable([]);
+
+export const systemsView = writable([]);
+export const systemsPage = writable(1);
+export const systemsTotalPages = writable(1);
+export const gamesView = writable([]);
+export const gamesPage = writable(1);
+export const gamesTotalPages = writable(1);
+export const romsView = writable([]);
+export const romsPage = writable(1);
+export const romsTotalPages = writable(1);
+export const pageSize = writable(20);
