d9813f473d1610a1cf7dc2a1255283bd78cf0845
Maxime Gauduin <alucryd@archlinux.org>
2023-06-03 12:25:45 +0200

    test purge-systems

---
+++ src/purge_systems/test_purge_systems.rs
@@ -0,0 +1,73 @@
+use super::super::config::*;
+use super::super::database::*;
+use super::super::import_dats;
+use super::super::import_roms;
+use super::*;
+use async_std::fs;
+use async_std::path::PathBuf;
+use tempfile::{NamedTempFile, TempDir};
+
+#[async_std::test]
+async fn test() {
+    // given
+    let _guard = MUTEX.lock().await;
+
+    let test_directory = Path::new("tests");
+    let progress_bar = ProgressBar::hidden();
+
+    let db_file = NamedTempFile::new().unwrap();
+    let pool = establish_connection(db_file.path().to_str().unwrap()).await;
+    let mut connection = pool.acquire().await.unwrap();
+
+    let rom_directory = TempDir::new_in(&test_directory).unwrap();
+    set_rom_directory(PathBuf::from(rom_directory.path()));
+    let tmp_directory = TempDir::new_in(&test_directory).unwrap();
+    let tmp_directory = set_tmp_directory(PathBuf::from(tmp_directory.path()));
+
+    let matches = import_dats::subcommand()
+        .get_matches_from(&["import-dats", "tests/Test System (20200721).dat"]);
+    import_dats::main(&mut connection, &matches, &progress_bar)
+        .await
+        .unwrap();
+
+    let romfile_names = vec![
+        "Test Game (Asia).rom",
+        "Test Game (Japan).rom",
+        "Test Game (USA, Europe).rom",
+        "Test Game (USA, Europe) (Beta).rom",
+    ];
+    for romfile_name in &romfile_names {
+        let romfile_path = tmp_directory.join(romfile_name);
+        fs::copy(test_directory.join(romfile_name), &romfile_path)
+            .await
+            .unwrap();
+        let matches = import_roms::subcommand()
+            .get_matches_from(&["import-roms", romfile_path.as_os_str().to_str().unwrap()]);
+        import_roms::main(&mut connection, &matches, &progress_bar)
+            .await
+            .unwrap();
+    }
+
+    let systems = find_systems(&mut connection).await;
+
+    // when
+    purge_system(&mut connection, &progress_bar, systems.first().unwrap())
+        .await
+        .unwrap();
+
+    // then
+    let systems = find_systems(&mut connection).await;
+    assert_eq!(systems.len(), 0);
+
+    let games = find_games(&mut connection).await;
+    assert_eq!(games.len(), 0);
+
+    let roms = find_roms(&mut connection).await;
+    assert_eq!(roms.len(), 0);
+
+    let romfiles = find_romfiles(&mut connection).await;
+
+    for romfile in romfiles {
+        assert!(romfile.path.contains("Trash"))
+    }
+}
--- test.sh
+++ test.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS='-Cinstrument-coverage'
-export LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw'
-
-cargo test --features benchmark,server
-grcov . --binary-path ./target/debug -s . -t lcov --branch --ignore-not-existing --llvm --filter covered --ignore "/*" -o ./lcov.info
-grcov . --binary-path ./target/debug -s . -t html --branch --ignore-not-existing --llvm --filter covered --ignore "/*" -o ./target/coverage/html
-rm *.profraw
+cargo llvm-cov --features benchmark,server --lcov --output-path lcov.info
+cargo llvm-cov report --open
