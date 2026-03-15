124c9a8206a635d9f4a4c2b3fbfa2ee69c9e5b87
Jaime Cura <jaimecura@gmail.com>
2026-03-15 01:18:38 +0100

    handle errors on import and check gracefully

--- src/check_roms.rs
+++ src/check_roms.rs
@@ -17,6 +17,7 @@ use super::sevenzip::AsArchive;
 use super::util::*;
 use clap::{Arg, ArgAction, ArgMatches, Command};
 use indicatif::ProgressBar;
+use simple_error::SimpleError;
 use simple_error::SimpleResult;
 use sqlx::sqlite::SqliteConnection;
 use std::collections::HashMap;
@@ -304,7 +305,9 @@ async fn check_archive(
         let rom = roms
             .iter()
             .find(|rom| rom.name == archive_romfile.path)
-            .unwrap();
+            .ok_or_else(|| {
+                SimpleError::new(format!("file not found in DB {}", archive_romfile.path))
+            })?;
         archive_romfile
             .check(connection, progress_bar, header, &[rom])
             .await?;
